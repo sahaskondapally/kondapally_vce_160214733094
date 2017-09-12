0bd3dcb9fd722ecec44e07489012255f0ad4e0ee
sahaskondapally <kondapallysahas@gmail.com>
2017-09-12 18:12:42 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -219,7 +219,7 @@ public class StudentGroup implements StudentArrayOperation {
 		for(int i=0;i<this.students.length-1;i++)
 		{
 			Date d=students[i].getBirthDate();
-			if(d.compareTo(firstDate)>0&& d.compareTo(lastDate)<0)
+			if(d.compareTo(firstDate)>=0&& d.compareTo(lastDate)<=0)
 				{
 				s[j]=students[i];
 				j++;
@@ -234,6 +234,7 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public Student[] getNearBirthDate(Date date, int days) {
+		
 		// Add your implementation here
 		return null;
 	}
@@ -258,7 +259,18 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public Student getNextStudent(Student student) {
+		if(student==null)
+			throw new IllegalArgumentException();
+		int j=0;
+		for(int i=0;i<this.students.length-1;i++)
+			if(this.students[i].equals(student))
+			{
+				j=i;
+				break;
+			}
+		if(j==this.students.length-1)
+			throw new IllegalArgumentException();
 		// Add your implementation here
-		return null;
+		return students[j+1];
 	}
 }
